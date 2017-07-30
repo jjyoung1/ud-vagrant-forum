ddbc63e364a8f2834391cab4ed4923dbcfe3c0fd
Joel Young <joel@jyng.com>
2017-07-30 19:53:44 -0400

    code transfer

--- DevelopingWithFrameworks/Flask/project.py
+++ DevelopingWithFrameworks/Flask/project.py
@@ -2,11 +2,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/hello')
 def hello_world():
-    return 'Hello World!'
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '<br>'
+    return output
 
 
 if __name__ == '__main__':
