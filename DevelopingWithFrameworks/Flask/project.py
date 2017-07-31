ad0deab34e4c033a98cb7ef689676496d74b398c
Joel Young <joel@jyng.com>
2017-07-30 20:57:11 -0400

    Database integrated

--- DevelopingWithFrameworks/Flask/project.py
+++ DevelopingWithFrameworks/Flask/project.py
@@ -1,20 +1,18 @@
 from flask import Flask
-
-app = Flask(__name__)
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+app = Flask(__name__)
+
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
 @app.route('/hello')
-def hello_world():
+def HelloWorld():
     restaurant = session.query(Restaurant).first()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     output = ''
