b9a1f08665b6da02c6b745b1ed930acbc4df5190
Joel Young <joel@jyng.com>
2017-08-01 17:55:35 -0400

    Empty Templates made /restaurants implemented

--- DevelopingWithFrameworks/Flask/finalProject.py
+++ DevelopingWithFrameworks/Flask/finalProject.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'},
+               {'name':'Blue Burgers', 'id':'2'},
+               {'name':'Taco Hut', 'id':'3'}]
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'},
+          {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},
+          {'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},
+          {'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},
+          {'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+
 
 @app.route('/restaurants')
-def restaurants():
-    return "restaurant list"
+def list_restaurants():
+    return render_template('restaurants.html', restaurants=restaurants)
 
 
 @app.route('/restaurant/new')
@@ -53,3 +68,6 @@ if __name__ == '__main__':
     app.secret_key = '4b$3H7of7E!-KyU#QCTw#c7Bdj_4#5'
     app.debug = True
     app.run('0.0.0.0', port=8080)
+
+
+
